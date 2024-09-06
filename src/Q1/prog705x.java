148286f62d87c22e3275d3f9a2d5383ef4dd2439
sookrajj <144150804+sookrajj@users.noreply.github.com>
2024-09-06 13:43:30 -0500

    hi

--- src/Q1/prog705x.java
+++ src/Q1/prog705x.java
@@ -5,30 +5,37 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public prog705x {
-
-  public static class hold {
-    private int series;
-    private String cha;
-
-    public hold(int nums, String character) {
-      series = nums;
-      cha = character;
-    }
-
-    public int getSeries() {return series;}
-    pubilc String getCha() {return cha;}
-  }
   public static void main(String[] args) {
     try {
-            Scanner s = new Scanner(new File("Langdat/prog512h.dat"));
-            Scanner stuff = new Scanner(new File("Langdat/prog705x.txt"));
-            ArrayList<hold> spe = new ArrayList<>();
+            Scanner s = new Scanner(new File("Langdat/prog705x.txt"));
+            Scanner stuff = new Scanner(new File("Langdat/prog512h.dat"));
+            ArrayList<int> spe = new ArrayList<>();
             ArrayList<String> poem = new ArrayList<>();
 
 
             while (s.hasNext()) {
-
+                spe.add(new hold(s.nextInt()));
             }
+            while (stuff.hasNext()) {
+              poem.add(stuff.nextLine());
+            }
+            int hi = 0;
+            for (hold ho : spe) {
+              if (ho%100 > hi) {
+                hi = ho%100;
+              }
+            }
+            if (poem.size() < hi) {
+              for (int lcv = 0; lcv < hi - poem.size(); lcv++) {
+                if (lcv > poem.size()) {
+                  poem.add(poem.get(lcv%poem.size()));
+                } else {
+                  poem.add(poem.get(lcv));
+                }
+              }
+            }
+            ArrayList<int> line = new ArrayList<>();
+            
 
 
 
