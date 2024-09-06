5ea6518172cbded23d9209b588258eb963971b94
sookrajj <sookraj.j@stu.janesville.k12.wi.us>
2024-09-06 12:09:20 -0500

    hi

--- src/Q1/prog505b.java
+++ src/Q1/prog505b.java
@@ -23,18 +23,104 @@ public class prog505b {
 
     public double getAve() {return ave;}
   }
-  public class strgrad {
+  public static class strgrad {
       private String f;
       private String l;
+      private int[] num;
       private double ave;
+
+      public strgrad(String first, String last, int[] nums) {
+          f = first;
+          l = last;
+          num = nums;
+          ave = 0;
+          calc();
+      }
+
+      private void calc() {
+          double total = 0;
+          for (int n : num) {
+              total += n;
+          }
+          ave = total/num.length;
+      }
+
+      public String getF() {return f;}
+
+      public String getL() {
+          return l;
+      }
+
+      public double getAve() {
+          return ave;
+      }
+
+      public int[] getNum() {
+          return num;
+      }
   }
 
   public static void main(String[] args) {
     try {
         Scanner s = new Scanner(new File("Langdat/prog505b.dat"));
-
+        ArrayList<strgrad> peps = new ArrayList<>();
         String f = "";
         String l = "";
+        int[] nums = new int[5];
+        while(s.hasNext()) {
+            f = s.nextLine();
+            l = s.nextLine();
+            nums[0] = s.nextInt();
+            nums[1] = s.nextInt();
+            nums[2] = s.nextInt();
+            nums[3] = s.nextInt();
+            nums[4] = s.nextInt();
+            peps.add(new strgrad(f, l, nums));
+        }
+        double[] exams = new double[5];
+        int[] grades = new int[5];
+        for (int lcv = 0; lcv < peps.size(); lcv++) {
+            System.out.print(peps.get(lcv).getF() + " " + peps.get(lcv).getL());
+            double ex1 = peps.get(lcv).getNum()[0];
+            double ex2 = peps.get(lcv).getNum()[1];
+            double ex3 = peps.get(lcv).getNum()[2];
+            double ex4 = peps.get(lcv).getNum()[3];
+            double ex5 = peps.get(lcv).getNum()[4];
+            System.out.print("\t\t" + ex1 + "\t" + ex2 + "\t" + ex3 + "\t" + ex4 + "\t" + ex5);
+            double hi = peps.get(lcv).getAve();
+            if (hi > 90) {
+                grades[0]++;
+                System.out.print("\t\t" + hi + "  A\n");
+            } else if (hi > 80) {
+                grades[1]++;
+                System.out.print("\t\t" + hi + "  B\n");
+            } else if (hi > 70) {
+                grades[2]++;
+                System.out.print("\t\t" + hi + "  C\n");
+            } else if (hi > 60) {
+                grades[3]++;
+                System.out.print("\t\t" + hi + "  D\n");
+            } else {
+                grades[4]++;
+                System.out.print("\t\t" + hi + "  F\n");
+            }
+            exams[0] = ex1;
+            exams[1] = ex2;
+            exams[2] = ex3;
+            exams[3] = ex4;
+            exams[4] = ex5;
+        }
+
+        exams[0] /= peps.size();
+        exams[1] /= peps.size();
+        exams[2] /= peps.size();
+        exams[3] /= peps.size();
+        exams[4] /= peps.size();
+        System.out.println("\n\nTest 1:\t\t" + exams[0] + "\t\tA: " + grades[0]);
+        System.out.println("\n\nTest 2:\t\t" + exams[1] + "\t\tB: " + grades[1]);
+        System.out.println("\n\nTest 3:\t\t" + exams[2] + "\t\tC: " + grades[2]);
+        System.out.println("\n\nTest 4:\t\t" + exams[3] + "\t\tD: " + grades[3]);
+        System.out.println("\n\nTest 5:\t\t" + exams[4] + "\t\tF: " + grades[4]);
 
     } catch (IOException e) {
         System.out.println("Can't find data file.");
