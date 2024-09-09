d153130f029e7e807de5d2a5852f3e9afe50847a
sookrajj <sookraj.j@stu.janesville.k12.wi.us>
2024-09-09 14:37:12 -0500

    hi

--- src/Q1/prog505b.java
+++ src/Q1/prog505b.java
@@ -4,30 +4,35 @@ import java.io.IOException;
 import java.util.*;
 
 public class prog505b {
-  public class prog505bcl {
-    private double[] test;
-    private double ave;
-
-    public prog505bcl(double[] tests) {
-      test = tests;
-      calc();
-    }
-
-    private void calc() {
-      double total = 0;
-      for (double i : test) {
-        total += i;
-      }
-      ave = total/test.length;
-    }
-
-    public double getAve() {return ave;}
-  }
+//  public class prog505bcl {
+//    private double[] test;
+//    private double ave;
+//
+//    public prog505bcl(double[] tests) {
+//      test = tests;
+//      calc();
+//    }
+//
+//    private void calc() {
+//      double total = 0;
+//      for (double i : test) {
+//        total += i;
+//      }
+//      ave = total/test.length;
+//    }
+//
+//    public double getAve() {return ave;}
+//  }
   public static class strgrad {
       private String f;
       private String l;
       private int[] num;
       private double ave;
+      public strgrad() {
+          f ="";
+          l = "";
+          ave = 0;
+      }
 
       public strgrad(String first, String last, int[] nums) {
           f = first;
@@ -67,25 +72,28 @@ public class prog505b {
         String f = "";
         String l = "";
         int[] nums = new int[5];
+        strgrad str = new strgrad();
         while(s.hasNext()) {
-            f = s.nextLine();
-            l = s.nextLine();
+            f = s.next();
+            l = s.next();
             nums[0] = s.nextInt();
             nums[1] = s.nextInt();
             nums[2] = s.nextInt();
             nums[3] = s.nextInt();
             nums[4] = s.nextInt();
-            peps.add(new strgrad(f, l, nums));
+            str = new strgrad(f, l, nums);
+            peps.add(str);
         }
+
         double[] exams = new double[5];
         int[] grades = new int[5];
         for (int lcv = 0; lcv < peps.size(); lcv++) {
             System.out.print(peps.get(lcv).getF() + " " + peps.get(lcv).getL());
-            double ex1 = peps.get(lcv).getNum()[0];
-            double ex2 = peps.get(lcv).getNum()[1];
-            double ex3 = peps.get(lcv).getNum()[2];
-            double ex4 = peps.get(lcv).getNum()[3];
-            double ex5 = peps.get(lcv).getNum()[4];
+            int ex1 = peps.get(lcv).getNum()[0];
+            int ex2 = peps.get(lcv).getNum()[1];
+            int ex3 = peps.get(lcv).getNum()[2];
+            int ex4 = peps.get(lcv).getNum()[3];
+            int ex5 = peps.get(lcv).getNum()[4];
             System.out.print("\t\t" + ex1 + "\t" + ex2 + "\t" + ex3 + "\t" + ex4 + "\t" + ex5);
             double hi = peps.get(lcv).getAve();
             if (hi > 90) {
@@ -104,23 +112,23 @@ public class prog505b {
                 grades[4]++;
                 System.out.print("\t\t" + hi + "  F\n");
             }
-            exams[0] = ex1;
-            exams[1] = ex2;
-            exams[2] = ex3;
-            exams[3] = ex4;
-            exams[4] = ex5;
+            exams[0] += ex1;
+            exams[1] += ex2;
+            exams[2] += ex3;
+            exams[3] += ex4;
+            exams[4] += ex5;
         }
 
-        exams[0] /= peps.size();
-        exams[1] /= peps.size();
-        exams[2] /= peps.size();
-        exams[3] /= peps.size();
-        exams[4] /= peps.size();
-        System.out.println("\n\nTest 1:\t\t" + exams[0] + "\t\tA: " + grades[0]);
-        System.out.println("\n\nTest 2:\t\t" + exams[1] + "\t\tB: " + grades[1]);
-        System.out.println("\n\nTest 3:\t\t" + exams[2] + "\t\tC: " + grades[2]);
-        System.out.println("\n\nTest 4:\t\t" + exams[3] + "\t\tD: " + grades[3]);
-        System.out.println("\n\nTest 5:\t\t" + exams[4] + "\t\tF: " + grades[4]);
+        exams[0] /= 3;
+        exams[1] /= 3;
+        exams[2] /= 3;
+        exams[3] /= 3;
+        exams[4] /= 3;
+        System.out.println("\nTest 1:\t\t" + exams[0] + "\t\tA: " + grades[0]);
+        System.out.println("\nTest 2:\t\t" + exams[1] + "\t\tB: " + grades[1]);
+        System.out.println("\nTest 3:\t\t" + exams[2] + "\t\tC: " + grades[2]);
+        System.out.println("\nTest 4:\t\t" + exams[3] + "\t\tD: " + grades[3]);
+        System.out.println("\nTest 5:\t\t" + exams[4] + "\t\tF: " + grades[4]);
 
     } catch (IOException e) {
         System.out.println("Can't find data file.");
