0a49e880bb7480e860da51b05bbb21980370e486
sookrajj <sookraj.j@stu.janesville.k12.wi.us>
2024-09-11 12:18:08 -0500

    hi

--- src/Q1/prog1050aincl.java
+++ src/Q1/prog1050aincl.java
@@ -36,6 +36,10 @@ public class prog1050aincl {
             System.out.println("Total profit on meat: " + moneyformat.format(computeSum(records, 2, "Meat", 13)));
             System.out.println("High priority sales percentage: " + computePercentage(records, 4, "H") + "%");
             System.out.println("Fruit loss in 2012: " + moneyformat.format(computeProfitLostIn2012(records, "Fruits")));
+            System.out.println("High priority sales shipped more than 3 days late: " + computeHighPrioLate(records));
+            System.out.println("Country with highest profit on Personal Care: " + computeHighestProfit(records, 2, "Personal Care"));
+            System.out.println("Region that bought the most snacks: " + computeMaxByField(records, 2, "Snacks", 0));
+            System.out.println("Sales to Africa: " + computeCount(records, 0, "Sub-Saharan Africa"));
 
         }
     }
@@ -65,6 +69,16 @@ public class prog1050aincl {
 
     public static double computeCountdec(List<SalesRecord> records, int fIndex, String value) {
         return ((double) computeCount(records, fIndex, value) / records.size()) *100;
+    }
+    public static int computeUnitsSold(List<SalesRecord> records, int fIndex1, String value1) {
+        int total = 0;
+        for (var record : records) {
+            if (record.fields[fIndex1].equalsIgnoreCase(value1)) {
+                total += (int) record.getUnitsSold();
+            }
+        }
+
+        return total;
     }
 
     public static int computeUnitsSold(List<SalesRecord> records, int fIndex1, String value1,
@@ -118,6 +132,103 @@ public class prog1050aincl {
         }
         return lostProfit;
 
+    }
+
+    public static int computeHighPrioLate(List<SalesRecord> records) {
+        int count = 0;
+        var dateformat = new SimpleDateFormat("M/d/yyyy");
+        for (var record: records) {
+            if (record.fields[4].equalsIgnoreCase("H")) {
+                try {
+                    Date od = dateformat.parse(record.fields[5]);
+                    Date sd = dateformat.parse(record.fields[7]);
+
+                    //Calc diff in days between ord and ship
+                    long diffInMs = Math.abs(sd.getTime() - od.getTime());
+                    long diffInDays = diffInMs / (1000*60*60*24);
+
+                    if (diffInDays > 3) count++;
+                } catch (ParseException e) {e.printStackTrace();}
+            }
+        }
+
+        return count;
+    }
+
+    public static String computeHighestProfit(List<SalesRecord> records, int fIndex, String itemType) {
+        String highestcountry = "N/A";
+        double highpro = 0;
+        for (var record : records) {
+            if (record.fields[fIndex].equalsIgnoreCase(itemType)) {
+                double profit = Double.parseDouble(record.fields[13]);
+                if (profit > highpro) {
+                    highpro = profit;
+                    highestcountry = record.fields[1];
+                }
+            }
+        }
+
+        return highestcountry;
+    }
+
+    public static String computeMaxByField(List<SalesRecord> records, int fIndex, String itemType, int resultFIndex) {
+        String topRegion = "N/A";
+        int maxCount = 0;
+        var regions = new ArrayList<String>();
+
+        // get list of unique regions
+        for (var record : records) {
+            if (record.fields[fIndex].equalsIgnoreCase(itemType)) {
+                String region = record.fields[resultFIndex];
+                if (!regions.contains(region)) regions.add(region);
+            }
+        }
+
+        // count occurrences of each region
+        for (var region : regions) {
+            int temp = 0;
+
+            for (var record : records) {
+                if (record.fields[fIndex].equalsIgnoreCase(itemType) && record.fields[resultFIndex].equalsIgnoreCase(region)) {
+                    temp++;
+                }
+            }
+            if (maxCount < temp) {maxCount = temp;
+                topRegion = region;
+            }
+        }
+
+        return topRegion;
+    }
+
+    public static void removeKuwait(List<SalesRecord> records) {
+
+        for (int lcv = 0 ; lcv < records.size(); lcv++) {
+            if (records.get(lcv).fields[1].equalsIgnoreCase("Kuwait")) {
+                records.remove(lcv);
+                lcv--;
+            }
+        }
+    }
+
+    public static void limitUganda(List<SalesRecord> records) {
+
+        for (int lcv = 0; lcv < records.size(); lcv++) {
+            if (records.get(lcv).fields[1].equalsIgnoreCase("Uganda")) {
+                String reg = records.get(lcv).fields[0];
+                String cou = records.get(lcv).fields[0];
+                String type = records.get(lcv).fields[0];
+                String sales = records.get(lcv).fields[0];
+                String ordp = records.get(lcv).fields[0];
+                String od = records.get(lcv).fields[0];
+                String oID = records.get(lcv).fields[0];
+                String sd = records.get(lcv).fields[0];
+                int units = 100;
+                String rege = records.get(lcv).fields[0];
+
+                double cost = Double.parseDouble(records.get(lcv).fields[10]);
+            }
+        }
     }
 }
 
