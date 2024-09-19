d755bd5f5b6d67f178d6abb428c26560220509be
Sookrajj <sookraj.j@stu.janesville.k12.wi.us>
2024-09-18 22:20:03 -0500

    hi

--- src/Q1/Tester.java
+++ src/Q1/Tester.java
@@ -9,6 +9,17 @@ public class Tester
         Scanner kbReader = new Scanner(System.in);
         System.out.print("Generate which term number? ");
         int k = kbReader.nextInt( );
-        //System.out.println("Term #" + k + " is " + ModFib.modFibonacci(k));
+        System.out.println("Term #" + k + " is " + modFibonacci(k));
+    }
+
+    public static int modFibonacci(int k) {
+        if (k == 0) return 3;
+        if (k == 1) return 5;
+        if (k == 2) return 8;
+        return modFibonacci(k-1) + modFibonacci(k-2) + modFibonacci(k-3);
     }
 }
+//Generate which term number? 5
+//Term #5 is 53
+//Generate which term number? 6
+//Term #6 is 98
---
+++ src/Q1/pelicanSort/sortTester.java
@@ -0,0 +1,57 @@
+package Q1.pelicanSort;
+
+public class sortTester {
+    public static void main(String[] args) {
+        int[] theArray = {4,2,5,1,3,18,0,9,6};
+        insertionSort(theArray);
+        for(int j = 0; j < theArray.length; j++) {
+            System.out.print(theArray[j] + " ");
+        }
+        System.out.println(" ");
+    }
+    public static void Bubblesort(int a[]) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                if (a[j] > a[i]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+
+            }
+        }
+    }
+    // Bubble sort
+    //0 1 2 3 4 5 6 9 18
+    public static void selectionSort(int[] a) {
+
+        for (int i = 0; i < a.length; i++) {
+            int low = i;
+            for (int j = i+1; j < a.length; j++) {
+                if (a[j] < a[low]) low =  j;
+            }
+            int temp = a[i];
+            a[i] = a[low];
+            a[low] = temp;
+        }
+    }
+    // Selection Sort
+    //0 1 2 3 4 5 6 9 18
+    public static void insertionSort(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+
+            for (int j = i-1; j >= 0; j--) {
+                if (a[j] > a[i]) {
+
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                    i--;
+
+                } else {break;}
+            }
+        }
+    }
+    // Insertion sort
+    //0 1 2 3 4 5 6 9 18
+}
