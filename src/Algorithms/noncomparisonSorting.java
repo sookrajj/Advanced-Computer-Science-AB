f3fbba4dc310dc485e6285274fa95abdeaac3ccb
sookrajj <sookraj.j@stu.janesville.k12.wi.us>
2024-09-20 14:35:15 -0500

    hi

--- src/Algorithms/noncomparisonSorting.java
+++ src/Algorithms/noncomparisonSorting.java
@@ -3,39 +3,65 @@ package Algorithms;
 import java.util.Arrays;
 
 public class noncomparisonSorting {
-    public static <T extends Comparable<T>> void radixSort(T[] arr, int d) {
-
-        int t = getDigit(arr[0], d);
-        System.out.println(t);
-    }
-
-    public static <T extends Comparable<T>> void radixSort(T[] arr) {
-        int numd = 0;
-        for (int lcv = 0; lcv < arr.length; lcv++) {
-            for (int l = 0; l < 10; l++) {
-                double po = Math.pow(10, l);
-                int p = (int) po;
-                System.out.println(p);
-                T num = (T) Integer.valueOf(p);
-                System.out.println(num);
-                if (arr[lcv].compareTo(num) > numd) {
-                    break;
-                }
-            }
+    public static <T extends Comparable<T>> T[] radixSort(T[] arr, int d) {
+        for (int digit = 1; digit < d; digit++) {
+            arr = countingSort(arr, d);
         }
-        radixSort(arr, numd);
+        return arr;
     }
-//procedure RadixSort(array, d) ▷ Sorts an array of numbers up to d digits
+    //procedure RadixSort(array, d) ▷ Sorts an array of numbers up to d digits
 //2: for digit ← 1 to d do
 //3: array ← CountingSortByDigit(array, digit)
 //4: end for
 //5: return array
 //6: end procedure
 //7:
-    public static <T extends Comparable<T>> void countingSort(T[] arr, int d) {
-        T[] output = arr;
-        Arrays.fill(output, null);
+    public static <T extends Comparable<T>> void radixSort(T[] arr) {
+        int numd = 0;
+        for (int lcv = 0; lcv < arr.length; lcv++) {
+            for (int l = 0; l < 10; l++) {
+                double po = Math.pow(10, l);
+                int p = (int) po;
+                T num = (T) Integer.valueOf(p);
+                if (arr[lcv].compareTo(num) > 0 && l > numd) {
+                    numd = l + 1;
+                    System.out.println(numd);
+                }
+            }
+        }
+        System.out.println(numd);
+        radixSort(arr, numd);
+    }
 
+    public static <T extends Comparable<T>> T[] countingSort(T[] arr, int d) {
+        Integer[] p = new Integer[arr.length];
+        T[] output = (T[]) p;
+        int[] cnt = new int[10];
+        System.out.println("hi");
+        for (int i = 0; i < arr.length-1; i++) {
+            int index = getDigit(arr[i], d);
+            cnt[index] = cnt[index]+1;
+        }
+        for (int lcv = 0; lcv < output.length; lcv++) {
+            System.out.print(output[lcv] + " ");
+
+        }
+        for (int i = 1; i < 9; i++) {
+            cnt[i] = cnt[i] + cnt[i-1];
+        }
+        for (int i = arr.length-1; i > -1; i--) {
+            int index = getDigit(arr[i], d);
+            System.out.println(index);
+
+            output[cnt[index] - 1] = arr[i];
+            cnt[index] = cnt[index] - 1;
+        }
+        for (int lcv = 0; lcv < output.length; lcv++) {
+            System.out.print(output[lcv] + " ");
+
+        }
+        System.out.println();
+        return output;
     }
 //8: procedure CountingSortByDigit(array, digit)
 //9: output ← new array of size length(array)
@@ -58,7 +84,7 @@ public class noncomparisonSorting {
     public static <T extends Comparable<T>> int getDigit(T n, int d) {
         int k = 0;
         Integer l = n.hashCode();
-        System.out.println(l);
+        k = (int) (l/Math.pow(10, d-1)) % 10;
         return k;
     }
 //26: procedure GetDigit(number, digit)
