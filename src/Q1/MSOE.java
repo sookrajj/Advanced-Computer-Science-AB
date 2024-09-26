b5eff09008715bd1da770e20283120367306d6ab
sookrajj <144150804+sookrajj@users.noreply.github.com>
2024-09-26 10:28:35 -0500

    hi

---
+++ src/Q1/MSOE.java
@@ -0,0 +1,21 @@
+package Q1;
+
+import java.util.*;
+
+public MSOE {
+  public static void main(String[] args) {
+    var input = new Scanner(System.in);
+    System.out.print("What is the length of the first shadow?  ");
+    double nums1 = input.nextInt();
+    System.out.print("\nWhat is the length of the second shadow?  ");
+    double nums2 = input.nextInt();
+    System.out.print("\nWhat is the height of the known height?  ");
+    double numh1 = input.nextInt();
+    if (nums1 < 0 || nums2 < 0 || numh1 < 0) {
+      System.out.print("\nThere is an invalid number");
+    else {
+      double numh2 = numh1 * nums2 / nums1;
+      System.out.print("\nThe height of the unknown height is: " + numh2);
+    }
+  }
+}
