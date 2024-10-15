eb3b98732085663069522aa59a3607bd0e3ee6a7
sookrajj <sookraj.j@stu.janesville.k12.wi.us>
2024-10-15 14:32:30 -0500

    hi

--- src/Q1/msoe20075.java
+++ src/Q1/msoe20075.java
@@ -10,26 +10,55 @@ public class msoe20075 {
         var input = new Scanner(System.in);
         System.out.println("Type in a sentence: ");
         var line = input.nextLine();
-        var longstr = "";
+        var longstr = new ArrayList<String>();
         var longest = 1;
         var check = 1;
-        var strs = new ArrayList<String>();
-        for (int lcv = 0; lcv < line.length()-1; lcv++) {
-            if (line.substring(lcv, lcv+1).equals(line.substring(lcv+1, lcv+2))) {
+        var strs = "";
+        for (int lcv = 0; lcv < line.length()-2; lcv++) {
+            if (line.substring(lcv, lcv+1).equals(line.substring(lcv+1, lcv+2)) && line.length()-lcv >= check) {
                 check = 2;
-                while (line.substring(lcv, lcv+1).equals(line.substring(lcv+1, lcv+2)) && lcv != line.length()) {
+                lcv++;
+                while (line.substring(lcv, lcv+1).equals(line.substring(lcv+1, lcv+2)) && lcv <= line.length()-2) {
                     lcv++;
                     check++;
                 }
-                if (check > longest) {
-                    longstr = line.substring(lcv-check+1, lcv+1);
-                    System.out.println(longstr);
+                if (check >= longest) {
+                    longstr.add(line.substring(lcv-check+1, lcv+1));
+                    longest = check;
+                    System.out.println(check);
                 }
             }
         }
-        strs.add("" + longest + "");
-        for (int lcv = 0; lcv < line.length()-longest; lcv++) {
-
+        var last = longstr.get(longstr.size()-1);
+        for (int lcv = 0; lcv < longstr.size(); lcv++) {
+            if (longstr.get(lcv).length() < last.length()) {
+                longstr.remove(lcv);
+                lcv--;
+            }
         }
+        strs = "" + longest + "";
+        for (int lcv = 0; lcv < line.length()-longest+1; lcv++) {
+            var let = line.substring(lcv, lcv+longest);
+            boolean iscombo = false;
+//            if (let.equals(line.substring(lcv+1, lcv+2))) {
+//                var temp = longest-2;
+//                for (int i = lcv+1; i < longest-2 && i < line.length()-1; i++) {
+//                    if (let.equals(line.substring(i, i+1))) {
+//                        temp--;
+//                        System.out.println(temp + "in");
+//                    }
+//                }
+//                if (temp == 0) iscombo = true;
+//            }
+            if (longstr.isEmpty()) {
+                break;
+            }
+            if (let.equals(longstr.get(0))) {
+                iscombo = true;
+                longstr.remove(0);
+            }
+            if (iscombo) strs += " " + let.substring(0, 1);
+        }
+        System.out.println(strs);
     }
 }
