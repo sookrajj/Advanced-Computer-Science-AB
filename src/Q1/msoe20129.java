4d3da634f88f38b307594467800517741f550560
sookrajj <144150804+sookrajj@users.noreply.github.com>
2024-10-15 15:22:21 -0500

    hi

---
+++ src/Q1/msoe20129.java
@@ -0,0 +1,16 @@
+package Q1;
+import java.util.*;
+
+public msoe20129 {
+  public static void main(String[] args) {
+    var input = new Scanner(System.in);
+    System.out.print("numeral");
+    var line = input.nextLine().split();
+    var num = 0;
+    for (int lcv = 0; lcv < line.size(); lcv++) {
+        var let = line[lcv];
+
+        
+    }
+  }
+}
