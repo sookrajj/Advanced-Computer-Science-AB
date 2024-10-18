1a4954a3f2b1cfc74d9883e9c5e97b1c66bb197a
sookrajj <144150804+sookrajj@users.noreply.github.com>
2024-10-18 09:32:49 -0500

    hi

---
+++ src/Q1/msoe/msoe20141.java
@@ -0,0 +1,18 @@
+package Q1.msoe;
+import java.util.*;
+
+public class msoe20141 {
+  public static void main(String[] args) {
+    var input = new Scanner(System.in);
+    System.out.print("Enter a word: ");
+    var str = input.nextLine();
+    str.toLowerCase();
+    var num = 0;
+    for (int lcv = 0; lcv < str.length()-1; lcv++) {
+      if (str.charAt(lcv).equals(str.charAt(lcv+1))) {
+        num++;
+      }
+    }
+    System.out.println("The number of double letters in " + str + " is " + num);
+  }
+}
