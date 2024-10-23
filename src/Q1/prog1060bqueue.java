f1a426695ba791aeed8e3333913561cc49560b92
sookrajj <sookraj.j@stu.janesville.k12.wi.us>
2024-10-23 14:37:00 -0500

    hi

--- src/Q1/prog1060bqueue.java
+++ src/Q1/prog1060bqueue.java
@@ -6,5 +6,39 @@ import java.util.*;
 public class prog1060bqueue {
     public static void main(String[] args) {
         Queue<Integer> que = new PriorityQueue<>();
+        var scanner = new Scanner(System.in);
+        var let = "";
+        do {
+            System.out.print("Enter task A)dd, D)elete, E)xit ");
+            let = scanner.next();
+            if (!let.equalsIgnoreCase("E")) {
+                System.out.print("Enter Number ");
+                Integer num = scanner.nextInt();
+                if (let.equalsIgnoreCase("A")) {
+                    que.add(num);
+                    System.out.println("This is the List: ");
+                    for (Integer n : que) {
+                        System.out.print(n + " ");
+                    }
+                    System.out.println();
+                } else {
+                    if (num.equals(que.peek())) {
+                        que.remove();
+                    } else {
+                        que.remove(num);
+                    }
+                    System.out.println("This is the List: ");
+                    for (Integer n : que) {
+                        System.out.print(n + " ");
+                    }
+                    System.out.println();
+                }
+            }
+        } while (!let.equalsIgnoreCase("E"));
+        System.out.println("This is the Ending List: ");
+        for (Integer n : que) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
     }
 }
