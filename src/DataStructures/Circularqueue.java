0a14236e28cdc3479d766eca97d0c131916f9596
sookrajj <sookraj.j@stu.janesville.k12.wi.us>
2024-11-07 12:17:37 -0600

    hi

---
+++ src/DataStructures/Circularqueue.java
@@ -0,0 +1,14 @@
+package DataStructures;
+
+@SuppressWarnings({"unchecked"})
+public class Circularqueue<T extends Comparable<T>> extends Queue<T> {
+    private T[] buffer;
+    private int head;
+    private int tail;
+    private int size;
+    private int capacity;
+
+    public void enqueue(Queue que, T element) {
+
+    }
+}
