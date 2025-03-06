84869c3336e7f9e6f14490823ac3f74118955ab0
sookrajj <sookraj.j@stu.janesville.k12.wi.us>
2025-03-06 12:17:56 -0600

    hi

--- src/Q3/prog595r.java
+++ src/Q3/prog595r.java
@@ -24,19 +24,20 @@ public class prog595r {
             for (String s : arr) {
                 pl.addAll(Arrays.asList(s.split("\\s+")));
             }
-            var better = pl.stream().map(s -> s.toLowerCase()).toList();
+            var better = pl.stream().map(s -> s.toLowerCase()).filter(s -> !s.equalsIgnoreCase("")).toList();
             var count = 0;
             var amount = 0;
             for (String value : better) {
-                System.out.println(value);
                 for (String s : set) {
                     count += s.equalsIgnoreCase(value) ? 1 : 0;
                 }
-                if (count == 0) amount++;
+                if (count == 0)  {
+                    amount++;
+                }
                 count = 0;
             }
             var score = 0.1579 * ((double)amount/ better.size() * 100) + 0.0496 * (double)better.size() / arr.length;
-            System.out.println(better.size() + " " + amount + " " + arr.length);
+            score += (double)amount/better.size() * 100 > 5 ? 3.6365 : 0;
             System.out.println("The Dale-Chall readability grade level is: " + score + " " + (score <= 4.9 ? "Grades K-4" : score <= 5.9 ? "Grades 5-6" : score <= 6.9 ? "Grades 7-8" :
                     score <= 7.9 ? "Grades 9-10" : score <= 8.9 ? "Grades 11-12" : score <= 9.9 ? "Grades 13-15 (College)" :
                     "Grades 16 and above (College Graduate)"));
@@ -49,3 +50,11 @@ public class prog595r {
         }
     }
 }
+//Enter a Text:
+//The quick brown fox jumps over the lazy dog. Many installations require thorough understanding.
+//The Dale-Chall readability grade level is: 9.622985714285715 Grades 13-15 (College)
+
+//Enter a Text:
+//
+//The Dale-Chall readability grade level is: NaN Grades 16 and above (College Graduate)
+
